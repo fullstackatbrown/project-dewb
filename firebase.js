49682ab78ab53c3c847decb9ee8ef04171ba66bc
catherine kim <catherine_m_kim@brown.edu>
2023-03-01 20:24:33 -0500

    init whatsapp clone w firebase

---
+++ firebase.js
@@ -0,0 +1,30 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
+import { getStorage } from "firebase/storage";
+import { initializeFirestore } from "firebase/firestore";
+const firebaseConfig = {
+  apiKey: "AIzaSyCtiSqB_puAJf0OUxdqkjgs5WUAp14XAp8",
+  authDomain: "tester-dewb.firebaseapp.com",
+  projectId: "tester-dewb",
+  storageBucket: "tester-dewb.appspot.com",
+  messagingSenderId: "29606429538",
+  appId: "1:29606429538:web:3235a2dd531e426db1623b"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const storage = getStorage(app);
+export const db = initializeFirestore(app, {
+  experimentalForceLongPolling: true,
+});
+
+export function signIn(email, password) {
+  return signInWithEmailAndPassword(auth, email, password);
+}
+export function signUp(email, password) {
+  return createUserWithEmailAndPassword(auth, email, password);
+}
--- screens/SignIn.js
+++ screens/SignIn.js
@@ -1,82 +1,87 @@
-import { StyleSheet, Text, View, TextInput, TouchableOpacity, Image } from 'react-native'
-import React, { useState } from 'react'
-import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import React, { useContext, useState } from "react";
+import { View, Text, Image, TextInput, Button } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import Context from "../context/Context";
+import { signIn, signUp } from "../firebase";
+export default function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [mode, setMode] = useState("signUp");
+  const {
+    theme: { colors },
+  } = useContext(Context);
 
-const SignIn = ({ navigation }) => {
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const handleSubmit = async () => {
-        if (name === '' || email === '' || password === '') {
-            alert("All fields are required");
-            return;
-        }
-        await axios.post("http://localhost:8001/api/signin", { name, email, password });
-        alert("Sign In Successful");
-    };
-    return (
-        <KeyboardAwareScrollView contentCotainerStyle={styles.container}>
-            <View style={{ marginVertical: 100 }}>
-            <View style={styles.imageContainer}>
-                <Image source={require("../assets/dewb_logo.png")} style={styles.imageStyles} />
-            </View>
-                <Text style={styles.signupText}>Sign In</Text>
-                <View style={{ marginHorizontal: 24 }}>
-                    <Text style={{ fontSize: 16, color: '#8e93a1' }}>EMAIL</Text>
-                    <TextInput style={styles.signupInput} value={email} onChangeText={text => setEmail(text)} autoCompleteType="email" keyboardType="email-address" />
-                </View>
-                <View style={{ marginHorizontal: 24 }}>
-                    <Text style={{ fontSize: 16, color: '#8e93a1' }}>PASSWORD</Text>
-                    <TextInput style={styles.signupInput} value={password} onChangeText={text => setPassword(text)} secureTextEntry={true} autoComplteType="password" />
-                </View>
-                <TouchableOpacity onPress={handleSubmit} style={styles.buttonStyle}>
-                    <Text style={styles.buttonText}>Submit</Text>
-                </TouchableOpacity>
-                <Text style={{ fontSize: 12, textAlign: "center" }}>
-                    Not yet registered? {" "}
-                    <Text style={{ color: "darkred", fontWeight: "bold" }}
-                        onPress={() => navigation.navigate("SignUp")}>
-                            Sign Up
-                        </Text>
-                </Text>
-                <Text style={{ fontSize: 12, textAlign: 'center', marginTop: 10 }}>Forgot Password?</Text>
-            </View>
-        </KeyboardAwareScrollView>
-    )
-}
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center'
-    },
-    signupText: {
-        fontSize: 30,
-        textAlign: 'center'
-    },
-    signupInput: {
-        borderBottomWidth: 0.5,
-        height: 48,
-        borderBottomColor: "#8e93a1",
-        marginBottom: 30,
-    },
-    buttonStyle: {
-        backgroundColor: "darkmagenta",
-        height: 50,
-        marginBottom: 20,
+  async function handlePress() {
+    if (mode === "signUp") {
+      await signUp(email, password);
+    }
+    if (mode === "signIn") {
+      await signIn(email, password);
+    }
+  }
+  return (
+    <View
+      style={{
         justifyContent: "center",
-        marginHorizontal: 15,
-        borderRadius: 15,
-    },
-    buttonText: {
-        fontSize: 20,
-        textAlign: 'center',
-        color: '#fff',
-        textTransform: 'uppercase',
-        fontWeight: 'bold'
-    },
-    imageContainer: { justifyContent: "center", alignItems: "center" },
-    imageStyles: { width: 100, height: 100, marginVertical: 20 }
-})
-
-export default SignIn
+        alignItems: "center",
+        flex: 1,
+        backgroundColor: colors.white,
+      }}
+    >
+      <Text
+        style={{ color: colors.foreground, fontSize: 24, marginBottom: 20 }}
+      >
+        Welcome to Whatsapp
+      </Text>
+      <Image
+        source={require("../assets/welcome-img.png")}
+        style={{ width: 180, height: 180 }}
+        resizeMode="cover"
+      />
+      <View style={{ marginTop: 20 }}>
+        <TextInput
+          placeholder="Email"
+          value={email}
+          onChangeText={setEmail}
+          style={{
+            borderBottomColor: colors.primary,
+            borderBottomWidth: 2,
+            width: 200,
+          }}
+        />
+        <TextInput
+          placeholder="Password"
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry={true}
+          style={{
+            borderBottomColor: colors.primary,
+            borderBottomWidth: 2,
+            width: 200,
+            marginTop: 20,
+          }}
+        />
+        <View style={{ marginTop: 20 }}>
+          <Button
+            title={mode === "signUp" ? "Sign Up" : "Sign in"}
+            disabled={!password || !email}
+            color={colors.secondary}
+            onPress={handlePress}
+          />
+        </View>
+        <TouchableOpacity
+          style={{ marginTop: 15 }}
+          onPress={() =>
+            mode === "signUp" ? setMode("signIn") : setMode("signUp")
+          }
+        >
+          <Text style={{ color: colors.secondaryText }}>
+            {mode === "signUp"
+              ? "Already have an account? Sign in"
+              : "Don't have an account? Sign Up"}
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
